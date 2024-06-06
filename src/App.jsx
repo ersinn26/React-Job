eb51f2c7776e034b3e228c9e0ee426e9e46607c5
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-06-06 23:23:55 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+
+import axios from "axios";
+
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
