190cedb4d2ab16bc509784e44e465d6dab5547a5
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-06-10 21:09:02 +0300

    app-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage.jsx";
+
+import {Navbar} from "./components/Navbar.jsx";
+import AllJobs from "./pages/AllJobs.jsx";
 
 import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import AddJob from "./pages/AddJob.jsx";
+
 function App() {
+
+   
+  const [jobs, setJobs] = useState([]);
+    
+  const getJobs = async () => {
+    try {
+      const res = await axios.get("/api/jobs");
+      const data = res.data;
+      console.log(data);
+      setJobs(data);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getJobs();
+  }, []);
+
+
+
+
   return (
     <>
-      <Navbar />
+     <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route index element={<HomePage />} />
+          <Route index element={<HomePage jobs={jobs} />} />
+          <Route path="/jobs" element={<AllJobs jobs={jobs} />} />
+          <Route path ="/add-job" element={<AddJob />} />
+
+          
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
     </>
   );
 }
